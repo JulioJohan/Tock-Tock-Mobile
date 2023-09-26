b2acda4572a019261c026ec7d0be194cf0a87cb4
JulioJohan <juliojohan10@gmail.com>
2023-09-25 23:39:26 -0600

    Agregando Archivo faltante

--- lib/presentation/screens/discover/discover_screen.dart
+++ lib/presentation/screens/discover/discover_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:toktok/presentation/providers/discover_provider.dart';
 import 'package:toktok/presentation/widgets/shared/video_scrollable_view.dart';
-import 'package:toktok/shared/data/local_video_posts.dart';
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
