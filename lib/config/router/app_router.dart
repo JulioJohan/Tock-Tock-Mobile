80b37f7ec721f110aeb4419ac32be7595d04a2a5
JulioJohan <juliojohan10@gmail.com>
2023-10-02 18:52:21 -0600

    Agregando cambios para las rutas

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,36 +1,38 @@
 import 'package:go_router/go_router.dart';
-import 'package:toktok/presentation/screens/discover/discover_screen.dart';
-import 'package:toktok/presentation/widgets/screens/login/login.dart';
-import 'package:toktok/presentation/widgets/screens/registro/registro.dart';
+import 'package:toktok/presentation/widgets/screens/home/home_screen.dart';
 
 
 final appRouter = GoRouter(
-  initialLocation: '/',
+  initialLocation: '/home/0',
   routes: [
     
     GoRoute(
-      path: '/',
-      name: LoginPage.name,
-      builder: (context, state) => LoginPage(),
-      routes: [
-         GoRoute(
-          path: '/home/:page',
-          name: DiscoverScreen.name,
-          builder: (context, state) => const DiscoverScreen()          
-        ),
-         GoRoute(
-          path: 'registro',
-          name: RegistroPage.name,
-          builder: (context, state) =>  RegistroPage()          
-        ),
-      ]
+      path: '/home/:page',
+      name: HomeScreen.name,
+      builder: (context, state) {
+        final pageIndex = int.parse( state.params['page'] ?? '0' );
+
+        return HomeScreen( pageIndex: pageIndex );
+      },
+      // routes: [
+      //    GoRoute(
+      //     path: 'movie/:id',
+      //     name: MovieScreen.name,
+      //     builder: (context, state) {
+      //       final movieId = state.params['id'] ?? 'no-id';
+
+      //       return MovieScreen( movieId: movieId );
+      //     },
+      //   ),
+      // ]
     ),
 
-// routes: {
-      //   '/': (context) => LoginPage(),
-      //   '/home': (context) =>  DiscoverScreen(),
-      // },
 
+
+    GoRoute(
+      path: '/',
+      redirect: ( _ , __ ) => '/home/0',
+    ),
 
   ]
 );
