75b554e99a04d8ffbb528a20cab0a49d3a1025d6
JulioJohan <juliojohan10@gmail.com>
2023-10-30 19:03:35 -0600

    Agregando CRUD del datasource

--- lib/domain/datasources/post_datasource.dart
+++ lib/domain/datasources/post_datasource.dart
@@ -1,8 +1,9 @@
+import 'package:toktok/infrastructure/models/post_dto.response.dart';
 import 'package:toktok/infrastructure/models/post_response.dart';
 
 abstract class PostDataSource {
   Future<Post> subtractLike(int idPost);
   Future<Post> sumLike(int idPost);
   Future<List<Post>> getAllPost(int page, int size);
-  Future<Post> savePost();
+  Future<Post> savePost(PostDto post);
 }
